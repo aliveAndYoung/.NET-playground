40bf8ed4871cd86bef9e4fbdeb378bb674520a81
Youssef <140279042+aliveAndYoung@users.noreply.github.com>
2025-08-24 01:50:01 +0300

    meh

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -13,8 +13,93 @@ namespace BookStore.Controllers
             List<Category> CategoriesList = _db.Categories.ToList();
             return View( CategoriesList );
         }
-        public IActionResult Create() {
-            return View(); 
+        public IActionResult Create()
+        {
+
+            return View();
         }
+        [HttpPost]
+        public IActionResult Create( Category obj )
+        {
+            if ( obj.Name == obj.DisplayOrder.ToString() )
+            {
+                ModelState.AddModelError( "name" , " the dispaly order and name can't match" );
+            }
+            if ( ModelState.IsValid )
+            {
+                _db.Categories.Add( obj );
+                _db.SaveChanges();
+                TempData[ "success" ] = "Category Created successfully";
+
+                return RedirectToAction( "Index" );
+
+            }
+            return View();
+        }
+
+
+
+        public IActionResult Edit( int? Id )
+        {
+            if ( Id == null || Id == 0 )
+            {
+
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault( c => c.Id == Id );
+            if ( category == null )
+            {
+                return RedirectToAction( "Index" );
+
+            }
+
+            return View( category );
+        }
+
+        [HttpPost]
+        public IActionResult Edit( Category obj )
+        {
+            if ( ModelState.IsValid )
+            {
+                _db.Categories.Update( obj );
+                _db.SaveChanges();
+                TempData[ "success" ] = "Category Edited successfully";
+
+                return RedirectToAction( "Index" );
+
+            }
+            return View();
+
+        }
+
+
+        public IActionResult Delete( int? id )
+        {
+            if ( id == null || id == 0 )
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find( id );
+
+            if ( categoryFromDb == null )
+            {
+                return NotFound();
+            }
+            return View( categoryFromDb );
+        }
+        [HttpPost, ActionName( "Delete" )]
+        public IActionResult DeletePOST( int? id )
+        {
+            Category? obj = _db.Categories.Find( id );
+            if ( obj == null )
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove( obj );
+            _db.SaveChanges();
+            TempData[ "success" ] = "Category deleted successfully";
+            return RedirectToAction( "Index" );
+        }
+
     }
 }
--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStore.Models
 {
@@ -7,7 +8,13 @@ namespace BookStore.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName( "Category Name" )]
+        [MaxLength( 100 )]
         public string Name { get; set; }
+
+        [DisplayName( "Display Order" )]
+        [Range(1 , 100 , ErrorMessage =" dsipaly order takes values in range 1 to 100")]
+
         public int DisplayOrder { get; set; }
     }
 }
